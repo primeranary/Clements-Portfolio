1d15f7f0498d79f984f1a445e14191a550e3055d
Clemy Ojie <Clement@Clementinas-MacBook-Air.local>
2018-11-30 16:57:04 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Clements-Portfolio/precache-manifest.094bef589669020690d1348d3fca64bb.js"
+  "n/precache-manifest.a5c18c0f249cd34d13ec1da07d6ac565.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Clements-Portfolio/index.html", {
+workbox.routing.registerNavigationRoute("n/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
