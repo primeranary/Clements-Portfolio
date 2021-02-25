935bd65286af155a3ef23e1faa5a34143ca0cd26
Clement Ojie <cojie48@gmail.com>
2021-02-24 21:57:49 -0500

    Updates

--- precache-manifest.4b0cd3db22a384b0badf8094fb06778e.js
+++ precache-manifest.4b0cd3db22a384b0badf8094fb06778e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Clements-Portfolio/static/js/runtime~main.4ea5772c.js"
   },
   {
-    "revision": "afb8c5e5e14850bd4ede",
-    "url": "/Clements-Portfolio/static/js/main.afb8c5e5.chunk.js"
+    "revision": "e179da0b5f675c6cddda",
+    "url": "/Clements-Portfolio/static/js/main.e179da0b.chunk.js"
   },
   {
     "revision": "0cd6058fc3c24877ba89",
     "url": "/Clements-Portfolio/static/js/1.0cd6058f.chunk.js"
   },
   {
-    "revision": "afb8c5e5e14850bd4ede",
+    "revision": "e179da0b5f675c6cddda",
     "url": "/Clements-Portfolio/static/css/main.3fa6cfbb.chunk.css"
   },
   {
-    "revision": "4c9c21ce052fce9363e0178d32fb2016",
+    "revision": "0ae2e6c11e17c836944b6fce9701f641",
     "url": "/Clements-Portfolio/index.html"
   }
 ];
